b9f803a242f11969eadb139ff5a19fcc5a399c67
Manish Shakya <mshakya@intrepidcs.com>
2024-07-24 03:53:35 -0400

    add more telescope extensions

--- lua/community.lua
+++ lua/community.lua
@@ -28,4 +28,6 @@ return {
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.recipes.telescope-lsp-mappings" },
+  { import = "astrocommunity.utility.telescope-live-grep-args-nvim"},
+  { import = "astrocommunity.file-explorer.telescope-file-browser-nvim"},
 }
