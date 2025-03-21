fc0703f02532684c8432003455d70b769a148394
Manish Shakya <mshakya@intrepidcs.com>
2025-03-21 15:52:18 -0400

    disable clang

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -45,8 +45,8 @@ return {
     config = {
       clangd = {
         on_attach = function(_, bufnr)
-          require("clangd_extensions.inlay_hints").setup_autocmd()
-          require("clangd_extensions.inlay_hints").set_inlay_hints()
+          -- require("clangd_extensions.inlay_hints").setup_autocmd()
+          -- require("clangd_extensions.inlay_hints").set_inlay_hints()
         end,
         capabilities = { offsetEncoding = "utf-8" },
       },
